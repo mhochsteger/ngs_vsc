917b7fe5ff9d618c644203536c1461372511b2a0
Henry v. Wahl <henry.wahl@univie.ac.at>
2022-07-11 15:52:26 +0200

    update mpi module

--- setup_env.sh
+++ setup_env.sh
@@ -3,7 +3,7 @@ module load \
 	       gcc/11.2.0-gcc-11.2.0-5i4t2bo\
 	       python/3.8.12-gcc-11.2.0-a5hqrgs \
 	       cmake/3.21.4-gcc-11.2.0-fmwwhz6 \
-	       openmpi/4.1.3-gcc-11.2.0-li5r24b \
+	       openmpi/4.1.4-gcc-11.2.0-nxdafup \
 	       intel-oneapi-mkl/2022.0.2-gcc-11.2.0-2ugv2zs
 
 PREFIX=~/ngs_vsc
